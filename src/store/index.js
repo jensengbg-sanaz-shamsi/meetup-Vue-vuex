ce591b11dba70d5338cf80e3ab361cd268e33aef
sanazshamsi <sanaz.shamsipour84@gmail.com>
2021-02-25 12:02:23 +0100

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,24 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    async fetchEvents(ctx) {
+      let options = {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Master-Key": ctx.state.apiKey,
+          "X-Bin-Versioning": "false"
+        }
+      }
+      let event = await ax.get(`${ctx.state.apiUrl}`, {
+        events: ctx.state.events
+      }, options)
+      ctx.commit('showEvents', event.data.events)
+
+    },
+    
+    async filteSearch(ctx, search) {
+      await ctx.commit('setFilterSearch', search)
+      ctx.dispatch('searchFilter')
   },
   modules: {
   }
